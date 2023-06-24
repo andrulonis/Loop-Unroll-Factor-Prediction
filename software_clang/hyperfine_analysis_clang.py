799035d19d072415ef000c56cbdc90d26c181703
andrulonis <48800417+andrulonis@users.noreply.github.com>
2023-06-25 01:03:04 +0200

    Update hyperfine_analysis_clang.py

--- software_clang/hyperfine_analysis_clang.py
+++ software_clang/hyperfine_analysis_clang.py
@@ -1,6 +1,6 @@
 import subprocess
 
-# Put this file in the directory of all benchmark programs' directories
+# Put this file in the directory of all benchmark programs' directories ("benchmark_codes")
 
 dirs = ["bb_gemm", "fft", "md", "pp_scan", "reduction", "ss_sort", "stencil", "triad"]
 
